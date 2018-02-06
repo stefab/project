9794584e305fcf8dacddb785a0c412e077b1dc5b
stepand <stepan.datsko@comodo.od.ua>
2018-02-07 01:51:16 +0200

    auth2

---
+++ EtsyRobot/Engine/JobModel/JobResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+    using System.ComponentModel.DataAnnotations;
+
+namespace EtsyRobot.Engine.JobModel
+{
+    public enum JobState
+    {
+        [Display(Name = "Not started")]
+        Incomplete = 0,
+        [Display(Name = "Completed")]
+        Completed = 1,
+        [Display(Name = "Completed with issues")]
+        CompletedWithIssues = 2,
+        [Display(Name = "Failed")]
+        Failed = 3
+    }
+
+    [JsonObject]
+    public sealed class GameResult
+    {
+        JobState Result = JobState.Incomplete;
+        [JsonProperty("processed")]
+        DateTime? Processed =  null;
+        [JsonProperty("error")]
+        string ErrorMessage;
+        [JsonProperty("elapsed")]
+        public double? ExecutionTime { get; set; }
+    }
+}
